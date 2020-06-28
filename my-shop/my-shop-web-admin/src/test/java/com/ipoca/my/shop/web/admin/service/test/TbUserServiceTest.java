e97065c0db9c727a9b8cc276b4345fef72b3e59e
ipocater <xugtbang128>
2020-06-28 18:02:05 +0800

    本地上传GitHub3

---
+++ my-shop/my-shop-web-admin/src/test/java/com/ipoca/my/shop/web/admin/service/test/TbUserServiceTest.java
@@ -0,0 +1,26 @@
+package com.ipoca.my.shop.web.admin.service.test;
+
+import com.ipoca.my.shop.domain.TbUser;
+import com.ipoca.my.shop.web.admin.dao.TbUserDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring-context.xml", "classpath:spring-context-druid.xml", "classpath:spring-context-mybatis.xml"})
+public class TbUserServiceTest {
+    @Autowired
+    private TbUserDao tbUserDao;
+
+    @Test
+    public void testSelectAll() {
+        List<TbUser> tbUsers = tbUserDao.selectAll();
+        for (TbUser tbUser : tbUsers) {
+            System.out.println(tbUser.getUsername());
+        }
+    }
+}
