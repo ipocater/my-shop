d2d506f4f085b99f6e4e065e5d98a3f5350b7d4a
ipocater <xugtbang128>
2020-07-01 17:09:48 +0800

    修改登录功能，使用数据库实现

--- my-shop/my-shop-web-admin/src/main/java/com/ipoca/my/shop/web/admin/web/interceptor/LoginInterceptor.java
+++ my-shop/my-shop-web-admin/src/main/java/com/ipoca/my/shop/web/admin/web/interceptor/LoginInterceptor.java
@@ -1,6 +1,7 @@
 package com.ipoca.my.shop.web.admin.web.interceptor;
 
 import com.ipoca.my.shop.commons.constant.ConstantUtils;
+import com.ipoca.my.shop.domain.TbUser;
 import com.ipoca.my.shop.domain.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        User user = (User)request.getSession().getAttribute(ConstantUtils.SESSION_USER);
+        TbUser user = (TbUser)request.getSession().getAttribute(ConstantUtils.SESSION_USER);
         if (user == null){
             response.sendRedirect("/login");
             return false;
